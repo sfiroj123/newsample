6f11273fd847ace8056c1b2cb18b6dd8b9c260e3
Firoj Shaik <firojshaik77@gmail.com>
2018-02-04 19:35:25 +0000

    commit

---
+++ src/test/java/com/simplemaven/framework/sampleselenium/SampleTest.java
@@ -0,0 +1,24 @@
+package com.simplemaven.framework.sampleselenium;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.ie.InternetExplorerDriver;
+import org.testng.annotations.Test;
+
+public class SampleTest {
+	@Test
+	public void Login()
+	
+	{
+		System.out.println("firoj is a good boy");
+	System.setProperty("webdriver.chrome.driver", "D:\\chromedriver.exe");
+		WebDriver driver= new ChromeDriver();
+		driver.get("https://www.google.co.uk");
+		
+		
+	}
+	
+	
+
+}
