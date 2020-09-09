88880cbf90710f038665449b1bf456757e11b80d
unknown <firoj.basha@gmail.com>
2020-09-09 23:23:41 +0100

    updated1

---
+++ src/test/java/com/simplemaven/framework/sampleselenium/Test1.java
@@ -0,0 +1,12 @@
+package com.simplemaven.framework.sampleselenium;
+
+public class Test1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println(" Second class");
+		
+
+	}
+
+}
