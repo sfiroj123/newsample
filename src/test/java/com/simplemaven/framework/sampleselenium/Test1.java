40e50ad2e73d9f0edac640824477512225c04d14
unknown <firoj.basha@gmail.com>
2020-09-09 23:37:52 +0100

    udpated test1

--- src/test/java/com/simplemaven/framework/sampleselenium/Test1.java
+++ src/test/java/com/simplemaven/framework/sampleselenium/Test1.java
@@ -5,6 +5,7 @@ public class Test1 {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println(" Second class");
+		System.out.println(" New udpate class");
 		
 
 	}
