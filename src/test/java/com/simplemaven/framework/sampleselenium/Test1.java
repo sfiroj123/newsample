186d02572cff7f53fbe27d8f4ac280029151eab3
Firoj Shaik <shaik.fj789@gmail.com>
2020-11-01 17:47:29 +0000

    udpated test1

--- src/test/java/com/simplemaven/framework/sampleselenium/Test1.java
+++ src/test/java/com/simplemaven/framework/sampleselenium/Test1.java
@@ -10,6 +10,7 @@ public class Test1 {
 		System.out.println(" GitHub updated at 11.37 am");
 
 		System.out.println(" Updated from Eclipse at 11.40 am");
+		System.out.println(" updated comment form test1 branch from local eclispe tool");
 //New BranchX commit
 	
 	}
